327274d40d2035dc8077a6799d7811c848d6a9a3
Rashid <rashidul.hasan@dsinnovators.com>
2021-10-31 13:37:38 +0600

    add Update and Delete endpoints for movies

--- src/main/java/com/dsi/spring/controller/MovieController.java
+++ src/main/java/com/dsi/spring/controller/MovieController.java
@@ -10,7 +10,10 @@ import com.dsi.spring.model.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -42,6 +45,37 @@ public class MovieController {
     @PostMapping("/add")
     public String addMovie(Movie movie) {
         movieDao.save(movie);
+        return "redirect:/admin/movies/";
+    }
+
+    // shows update form
+    @GetMapping("/edit/{id}")
+    public String updateMovieForm(@PathVariable("id") long id, Model model){
+        List<Cast> casts = castDao.findAll();
+        model.addAttribute("casts", casts);
+
+        Movie movie = movieDao.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid Movie id: " + id));
+        model.addAttribute("movieForm", movie);
+
+        return "admin/movie/add_movie_form";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String updateMovie(@PathVariable("id") long id, @Validated Movie movie, BindingResult result, Model model){
+        if (result.hasErrors()){
+            movie.setId(id);
+            return "admin/movie/add_movie_form";
+        }
+
+        movieDao.save(movie);
+        return "redirect:/admin/movies/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteMovie(@PathVariable("id") long id, Model model){
+        Movie movie = movieDao.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid Movie id: " + id));
+        movieDao.delete(movie);
+
         return "redirect:/admin/movies/";
     }
 }
