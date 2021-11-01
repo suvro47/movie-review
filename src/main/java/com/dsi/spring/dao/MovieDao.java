db0ae4e7d815119c934c4085c392ada8f679955b
jspw <mhshifat757@gmail.com>
2021-11-01 17:24:37 +0600

    removed unused imports

--- src/main/java/com/dsi/spring/dao/MovieDao.java
+++ src/main/java/com/dsi/spring/dao/MovieDao.java
@@ -1,7 +1,5 @@
 package com.dsi.spring.dao;
 
-import java.util.List;
-
 import com.dsi.spring.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
